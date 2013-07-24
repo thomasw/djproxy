03f88abe0e395a1beba0a497b046a20321bc720f
Thomas W <thomas@matchstrike.net>
2013-07-24 16:40:39 -0700

    First pass at setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+from djproxy import __version__, __author__
+
+setup(
+    name="djproxy",
+    version=__version__,
+    url='https://github.com/thomasw/djproxy',
+    download_url='https://github.com/thomasw/djproxy/releases',
+    author=__author__,
+    author_email='thomas.welfley+djproxy@gmail.com',
+    description='djproxy is a simple reverse proxy class-based generic view '
+                'for Django apps. If your application depends on proxies '
+                'provided by a web server in production, djproxy can be used '
+                'to duplicate that functionality during local development.',
+    packages=find_packages(),
+    tests_require=[
+        'mock==1.0.1', 'nose==1.3.0', 'pinocchio==0.3.1', 'pyflakes==0.7.3',
+        'unittest2==0.5.1', 'requests==1.2.3', 'django<1.6'],
+    install_requires=['requests<=1.0.0,<2.0.0', 'django<=1.3,<1.6'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries',
+    ],
+    test_suite='nose.collector'
+)
