70863101a882eee0811460cf9bf0f8442d9b0775
Michael Joseph <michaeljoseph+github@gmail.com>
2014-02-21 15:31:22 +0200

    Return a `patterns` rather than a list of tuples

--- djproxy/urls.py
+++ djproxy/urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls import patterns
+import re
+
+from django.conf.urls import patterns, url
+
 from djproxy.views import HttpProxy
 
 
 def generate_routes(config):
-    routes = []
+    routes = ()
     for service_name, proxy_config in config.items():
-        ProxyClass = type(
-            'ProxyClass',
-            (HttpProxy, ),
-            {'base_url': proxy_config['base_url']}
-        )
-        routes.append((
-            r'^%s' % proxy_config['prefix'],
-            ProxyClass.as_view()
-        ))
-    return routes
+        base_url = proxy_config['base_url']
+        prefix = proxy_config['prefix']
+
+        ProxyClass = type('ProxyClass', (HttpProxy,), {'base_url': base_url})
+
+        routes += url(r'^%s' % prefix.lstrip('/'), ProxyClass.as_view()),
+
+    return patterns('', *routes)
