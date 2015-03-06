2493acbe1ae2af89b3106b62f95bf6f6ae78d557
Thomas W <thomas@matchstrike.net>
2015-03-06 15:13:45 -0800

    Move installable description to docstring

--- djproxy/__init__.py
+++ djproxy/__init__.py
@@ -1,2 +1,8 @@
+"""
+djproxy is a simple reverse proxy class-based generic view for Django apps.
+
+If your application depends on proxies provided by a web server in production,
+djproxy can be used to duplicate that functionality during local development.
+"""
 __author__ = 'Thomas Welfley'
 __version__ = '2.1.0'
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-from djproxy import __version__, __author__
+from djproxy import __author__, __doc__, __version__
 
 # Multiprocessing is needed to execute `python setup.py tests` without any
 # errors in some installations.
@@ -17,15 +17,12 @@ setup(
     download_url='https://github.com/thomasw/djproxy/releases',
     author=__author__,
     author_email='thomas.welfley+djproxy@gmail.com',
-    description='djproxy is a simple reverse proxy class-based generic view '
-                'for Django apps. If your application depends on proxies '
-                'provided by a web server in production, djproxy can be used '
-                'to duplicate that functionality during local development.',
     packages=find_packages(),
     tests_require=[
         'mock==1.0.1', 'nose==1.3.0', 'pinocchio==0.3.1', 'pyflakes==0.7.3',
         'unittest2==0.5.1', 'requests>=1.0.0', 'django>=1.4'],
     install_requires=['requests>=1.0.0', 'django>=1.4'],
+    description=__doc__,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
