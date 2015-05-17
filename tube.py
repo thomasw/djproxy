d9bbd0490eeff06c26f6a427d479b026eee6ebf2
Thomas W <thomas@matchstrike.net>
2015-05-17 15:13:21 -0700

    Don't automatically run pep257 on tests.

--- tube.py
+++ tube.py
@@ -2,7 +2,7 @@ from testtube.helpers import Flake8, Frosted, Nosetests, Pep257
 
 
 PATTERNS = (
-    (r'((?!_tests\.py)(?!tube\.py).)*\.py$', [Pep257(bells=0)]),
+    (r'((?!tests)(?!tube\.py).)*\.py$', [Pep257(bells=0)]),
     (r'.*\.py$', [Flake8(all_files=True), Frosted(all_files=True)],
      {'fail_fast': True}),
     (r'(.*setup\.cfg$)|(.*\.coveragerc)|(.*\.py$)', [Nosetests()])
