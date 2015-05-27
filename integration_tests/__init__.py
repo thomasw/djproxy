37b437be2f121f5ebbb7f49b38a080db054f57a7
Thomas W <thomas@matchstrike.net>
2015-05-27 13:42:57 -0700

    Make django setup in tests django >1.7 compatible

--- integration_tests/__init__.py
+++ integration_tests/__init__.py
@@ -1,7 +1,14 @@
 import os
 
+import django
 from django.conf import settings
 
 # Configure django so that our tests work correctly
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
-settings._setup()
+
+try:
+    # This is how we setup django apps in scripts for django >1.7
+    django.setup()
+except AttributeError:
+    # This is how we do it for older versions of Django
+    settings._setup()
