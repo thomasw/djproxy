d321f33cee391c16c4e22dd80bb8f2912b7f0b77
Thomas W <thomas@matchstrike.net>
2016-03-10 16:44:58 -0800

    Setup: add missing classifier

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
