61ab69c961d85f695080c8f0862d18447e93302b
Thomas Welfley <thomas.welfley@gmail.com>
2018-11-30 09:55:51 -0800

    Drop Python 2.6 support

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+dev
+---
+
+- Drop support for Python 2.6
+
 2.3.4
 -----
 
--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from codecs import open
-import multiprocessing  # noqa `python setup.py test` fix for python 2.6
 from setuptools import setup, find_packages
-import sys
 
 from djproxy import __author__, __doc__, __version__
 
@@ -9,10 +7,6 @@ install_requires = ['requests>=1.0.0', 'django>=1.4', 'six>=1.9.0']
 tests_require = [
     'mock==1.3.0', 'nose==1.3.7', 'unittest2==1.1.0', 'spec==1.3.1',
     'requests>=1.0.0']
-
-# Django >= 1.7 is not 2.6 compatible
-if sys.version_info[:2] < (2, 7):
-    install_requires += ['django<1.7']
 
 with open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
@@ -34,15 +28,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries',
-        'Framework :: Django :: 1.4',
-        'Framework :: Django :: 1.5',
-        'Framework :: Django :: 1.6',
-        'Framework :: Django :: 1.7',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
